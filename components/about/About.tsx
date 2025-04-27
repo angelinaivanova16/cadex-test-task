60213876ddb53a8544b03607be24d5001b127aa3
Angelina <angelinaivanova16@gmail.com>
2025-04-27 16:07:22 +0300

    fix: fix styles

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,10 +1,17 @@
 'use client';
 
+import { useState } from 'react';
 import s from './about.module.scss';
 import dynamic from 'next/dynamic';
+
 const ReactPlayer = dynamic(() => import('react-player/youtube'), { ssr: false });
 
 export const About = () => {
+  const [videoError, setVideoError] = useState(false);
+
+  const handleVideoError = () => {
+    setVideoError(true);
+  };
 
   return (
     <section className={s.about}>
@@ -15,12 +22,20 @@ export const About = () => {
         </p>
       </div>
       <div className={s.video}>
-        <ReactPlayer
-          url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          width="100%"
-          height="100%"
-          controls={true}
-        />
+        {!videoError ? (
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            width="100%"
+            height="100%"
+            controls={true}
+            onError={handleVideoError}
+          />
+        ) : (
+          <div className={s.warning}>
+            <p>К сожалению, это видео недоступно в вашем регионе или при использовании определенных методов доступа.</p>
+            <p>Пожалуйста, попробуйте использовать VPN для просмотра.</p>
+          </div>
+        )}
       </div>
     </section>
   );
